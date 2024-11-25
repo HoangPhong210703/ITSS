a79056f0d1b0ee2e8c1a6735fe39a487f1d64ae5
hoangQuan25 <quandz250203@gmail.com>
2024-11-25 10:58:02 +0700

    add basic frame and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Homepage from './Homepage/Homepage';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import BlankLayout from "./components/BlankLayout";
+
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Homepage from "./pages/Homepage/Homepage";
 
 const App = () => {
   return (
-    <>
-      <Homepage />
-    </>
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route
+          path="/login"
+          element={
+            <BlankLayout>
+              <Login />
+            </BlankLayout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <BlankLayout>
+              <Register />
+            </BlankLayout>
+          }
+        />
+
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Homepage />
+            </MainLayout>
+          }
+        />
+        {/* <Route
+          path="/menu"
+          element={
+            <MainLayout>
+              <MenuPage />
+            </MainLayout>
+          }
+        /> */}
+      </Routes>
+    </Router>
   );
 };
 
---
+++ frontend/src/components/BlankLayout.js
@@ -0,0 +1,8 @@
+// components/BlankLayout.js
+import React from "react";
+
+const BlankLayout = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default BlankLayout;
